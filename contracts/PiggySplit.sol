e3562e04e25f1409bedded8d1b4f4808feec369b
Gauthier Petetin <gauthierpetetin@Gauthiers-MacBook-Pro.local>
2018-08-16 15:21:32 +0200

    PiggySplit MVP

--- contracts/PiggySplit.sol
+++ contracts/PiggySplit.sol
@@ -32,6 +32,8 @@ library SafeMath {
 
 contract Piggies {
 
+  address public owner;
+
   function withdraw(address _withDrawalAddress) public {}
 
 }
@@ -56,10 +58,10 @@ contract PiggySplit {
   /**
    * @dev Constructor
    * Anthony, David, Gauthier, Matthis, Pablo, Raphael, Sylvain
-   * ["0xb5747835141b46f7c472393b31f8f5a57f74a44f", "0x53799fa918c8b4c3e207f684575873e9c5f1b00c", "0xc6f0410a667a5bea528d6bc9efbe10270089bb11", "0xa1ee07042c67e2a0391ba2a418c8fd19522ba130", "0xed9f644c1228644e62a8e3911ca20afb48029e70", "0xc050ca762d1913ba642a8bec6220710667c34f9b", "0x158af3e880835664c396f5d28dab7ee9b73c206f"],["5000", "5000", "26500", "6750", "16750", "26500", "13500"]
+   * "0x5007e152e6017d5c331b41cc2636ff0495f360d1", ["0xb5747835141b46f7c472393b31f8f5a57f74a44f", "0x53799fa918c8b4c3e207f684575873e9c5f1b00c", "0xc6f0410a667a5bea528d6bc9efbe10270089bb11", "0xa1ee07042c67e2a0391ba2a418c8fd19522ba130", "0xed9f644c1228644e62a8e3911ca20afb48029e70", "0xc050ca762d1913ba642a8bec6220710667c34f9b", "0x158af3e880835664c396f5d28dab7ee9b73c206f"],["5000", "5000", "26500", "6750", "16750", "26500", "13500"]
    */
   constructor(address _piggyContractAddress, address[] _payees, uint256[] _shares) public payable {
-    require(_payee != address(0));
+    require(_piggyContractAddress != address(0));
     require(_payees.length == _shares.length);
     require(_payees.length > 0);
 
@@ -153,7 +155,7 @@ contract PiggySplit {
     returns (uint256 payeeShares)
   {
     require(shares[msg.sender] != 0);
-    payeeShares = shares[msg.sender].div(totalShares);
+    payeeShares = shares[msg.sender];
   }
 
   function getFunds() public view
@@ -162,6 +164,13 @@ contract PiggySplit {
     require(shares[msg.sender] != 0);
     uint256 totalReceived = address(this).balance.add(totalReleased);
     payeeFunds = totalReceived.mul(shares[msg.sender]).div(totalShares).sub(released[msg.sender]);
+  }
+
+  // Method to test if the connection has been established with the game contract
+  function getGameContractOwner() public view
+    returns (address gameContractOwner)
+  {
+    gameContractOwner = piggyContract.owner();
   }
 
 }
